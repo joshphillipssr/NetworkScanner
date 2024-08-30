9b3e673709db2505a4098ffe723db7babb9e1434
Josh Phillips <josh@joshphillipssr.com>
2024-08-30 11:26:14 -0500

    Cleaned up list function

--- network_scanner.py
+++ network_scanner.py
@@ -6,11 +6,10 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
+    print("IP\t\t\t\t\t\tMAC Address\n------------------------------------------")
     for element in answered_list:
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-        print("----------")
+        print(element[1].psrc + "\t\t\t" + element[1].hwsrc)
 
 scan("192.168.251.2/24")
