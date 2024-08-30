c9a1f3cc64ebe202350ee3712c8aa6690e09f7d6
Josh Phillips <josh@joshphillipssr.com>
2024-08-30 17:18:09 -0500

    Added prompt and optparse

--- network_scanner.py
+++ network_scanner.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target", help="Target IP or Network to scan.")
+    (options, arguments) = parser.parse_args()
+
+    if not options.target:
+        options.target = input("[*] Please enter the target IP or network to scan: ")
+
+    return options
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -8,8 +19,18 @@ def scan(ip):
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print("IP\t\t\t\t\t\tMAC Address\n------------------------------------------")
+    clients_list = []
     for element in answered_list:
-        print(element[1].psrc + "\t\t\t" + element[1].hwsrc)
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
 
-scan("192.168.251.2/24")
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n------------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+options = get_arguments()
+
+scan_result = scan(options.target)
+print_result(scan_result)
